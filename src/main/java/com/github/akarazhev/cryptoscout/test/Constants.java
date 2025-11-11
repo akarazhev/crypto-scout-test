8706e0d7ce75f1e84b03dc32e2ec5a9ceaa36ccb
Andrey Karazhev <karazhev@gmail.com>
2025-11-11 13:33:38 +0300

    Fix tests

--- src/main/java/com/github/akarazhev/cryptoscout/test/Constants.java
+++ src/main/java/com/github/akarazhev/cryptoscout/test/Constants.java
@@ -40,7 +40,7 @@ final class Constants {
         }
 
         static final String BYBIT_CRYPTO_STREAM = "bybit-crypto-stream";
-        static final int CONNECTION_ESTABLISHED_MS = 50;
+        static final int CONNECTION_ESTABLISHED_MS = 200;
     }
 
     final static class DB {

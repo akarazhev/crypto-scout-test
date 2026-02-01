1fc4d2df820a6d78e17f421c53a177ca24e5519d
Andrey Karazhev <karazhev@gmail.com>
2026-02-01 23:01:43 +0300

    Suppress unchecked warnings in MockCryptoScoutDataTest
    - Add @SuppressWarnings(unchecked) to class level
    - JSON data casting to generic types is intentional in test code

--- src/test/java/com/github/akarazhev/cryptoscout/test/MockCryptoScoutDataTest.java
+++ src/test/java/com/github/akarazhev/cryptoscout/test/MockCryptoScoutDataTest.java
@@ -55,6 +55,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SuppressWarnings("unchecked")
 final class MockCryptoScoutDataTest {
 
     @Test
